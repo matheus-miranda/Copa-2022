9d3c2511442e9020196cbabde20b075d10c06770
Matheus Miranda <matheus.sm86@gmail.com>
2022-11-16 23:45:23 -0300

    Rollback to suspend functions

--- domain/src/main/kotlin/me/dio/copa/catar/domain/repositories/MatchesRepository.kt
+++ domain/src/main/kotlin/me/dio/copa/catar/domain/repositories/MatchesRepository.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import me.dio.copa.catar.domain.model.Match
 
 interface MatchesRepository {
-    fun getMatches(): Flow<List<Match>>
+    suspend fun getMatches(): Flow<List<Match>>
     suspend fun enableNotificationFor(id: String)
     suspend fun disableNotificationFor(id: String)
 }
