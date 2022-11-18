b8c4166e8c24375cfb16abecd9f0dd54d6d1fa14
Matheus Miranda <matheus.sm86@gmail.com>
2022-11-18 00:08:01 -0300

    Create MainScreen

---
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
@@ -0,0 +1,176 @@
+package me.dio.copa.catar.features
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import me.dio.copa.catar.R
+import me.dio.copa.catar.domain.extensions.getDate
+import me.dio.copa.catar.domain.model.Match
+import me.dio.copa.catar.domain.model.MatchDomain
+import me.dio.copa.catar.domain.model.Stadium
+import me.dio.copa.catar.domain.model.Team
+import me.dio.copa.catar.domain.model.TeamDomain
+import me.dio.copa.catar.ui.theme.Copa2022Theme
+import me.dio.copa.catar.ui.theme.Shapes
+import me.dio.copa.catar.utils.DarkThemePreview
+import java.time.LocalDateTime
+
+@Composable
+fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    val uiState by viewModel.state.collectAsState()
+    MainScreen(uiState.matches)
+}
+
+@Composable
+fun MainScreen(matches: List<MatchDomain>) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            items(matches) { match ->
+                MatchInfo(match)
+            }
+        }
+    }
+}
+
+@Composable
+fun MatchInfo(match: MatchDomain) {
+    Card(
+        shape = Shapes.large,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box {
+            AsyncImage(
+                model = match.stadium.image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.height(160.dp)
+            )
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Notification(match)
+                Title(match)
+                Teams(match)
+            }
+        }
+    }
+}
+
+@Composable
+fun Notification(match: MatchDomain) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+        val drawable = if (match.notificationEnabled) R.drawable.ic_notifications_active
+        else R.drawable.ic_notifications
+
+        Image(
+            painter = painterResource(id = drawable),
+            modifier = Modifier.clickable {
+                // onClick(match)
+            },
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun Title(match: MatchDomain) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "${match.date.getDate()} - ${match.name}",
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+        )
+    }
+}
+
+@Composable
+fun Teams(match: MatchDomain) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        TeamItem(team = match.team1)
+
+        Text(
+            text = "X",
+            modifier = Modifier.padding(end = 16.dp, start = 16.dp),
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+        )
+
+        TeamItem(team = match.team2)
+    }
+}
+
+@Composable
+fun TeamItem(team: TeamDomain) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = team.flag,
+            modifier = Modifier.align(Alignment.CenterVertically),
+            style = MaterialTheme.typography.h3.copy(color = Color.White)
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(
+            text = team.displayName,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h6.copy(color = Color.White)
+        )
+    }
+}
+
+@DarkThemePreview
+@Composable
+fun PreviewMainScreen() {
+    val matches = listOf(
+        Match(
+            id = "",
+            name = "Final",
+            stadium = Stadium(
+                name = "Catar Stadim",
+                image = "https://digitalinnovationone.github.io/copa-2022-android/statics/lusali-stadium.png"
+            ),
+            team1 = Team(flag = "BR", displayName = "BRA"),
+            team2 = Team(flag = "AR", displayName = "ARG"),
+            date = LocalDateTime.now(),
+            notificationEnabled = true
+        )
+    )
+
+    Copa2022Theme {
+        MainScreen(matches = matches)
+    }
+}
